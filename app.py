de0bdc20383e7f7a0d3e7aca9a93b6e5d647c1e9
mario598712-collab <mario598712@gmail.com>
2026-03-22 00:29:35 -0600

    Update file path for STREAMLIT_VANTAGE_SIN_XAU.xlsx

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.caption(
 # RUTA RELATIVA DE TU ARCHIVO
 # =============================
 BASE = Path(__file__).parent
-RUTA_ESTRAT = BASE / "data" / "STREAMLIT_VANTAGE.xlsx"  # 👈 Solo un archivo
+RUTA_ESTRAT = BASE / "data" / "STREAMLIT_VANTAGE_SIN_XAU.xlsx"  # 👈 Solo un archivo
 
 # =============================
 # UTILIDADES
